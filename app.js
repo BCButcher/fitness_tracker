2106329a5627f794fcecddd42a27e67fd73c0343
Brian Butcher <butcher.brian.c@gmail.com>
2019-11-26 12:55:32 -0500

    aded app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var routes = require("./controller/controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+});
