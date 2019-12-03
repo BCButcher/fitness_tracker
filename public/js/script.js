acdbb0ff978a7282d348714f299098f364ae4276
Brian Butcher <butcher.brian.c@gmail.com>
2019-12-03 00:28:09 -0500

    updated script.js

--- public/js/script.js
+++ public/js/script.js
@@ -7,48 +7,49 @@ function writeExercises(id) {
         method: "GET",
         success: result => {
             for (exercise of result[0].exercises) {
-                $("#exerciseList").append(`<li class="col-6">${exercise.name} (Muscle Group: ${exercise.muscleGroup}) (Reps: ${exercise.reps})</li>`)
+                $("#exerciseList").append(`<li class="col-6">${exercise.name} (Reps: ${exercise.reps})</li>`)
             }
         }
     })
 }
 
 function writeExerciseForm(buttonTarget) {
-    $("#getInShape").append(`<h2>Add Muscle Group to ${$(buttonTarget).text()}</h2>
+    $("#getFit").append(`
+    <h2>Add Exercise to ${$(buttonTarget).text()}</h2>
     <form action="/add" method="post">
         <div class="form-group>
-            <label for="exerciseName">Exercise Name</label>
-            <input class="form-control" type="text" name="exerciseName">
-        </div>
-        <div class="form-group>
-            <label for="muscleGroup">Muscle Group</label>
-            <input class="form-control" type="text" name="muscleGroup">
+            <label for="exerciseName">Name of Exercise</label>
+            <input class="form-control" type="text" name="exerciseName" value="" placeholder="Exercise">
         </div>
         <div class="form-group">
-            <label for="exerciseReps">Reps</label>
-            <input class="form-control" type="number" name="exerciseReps">
+            <label for="exerciseReps">Number of Reps</label>
+            <input class="form-control" type="number" name="exerciseReps" value="" placeholder="# of Reps">
         </div>
-        <button class="btn btn-primary" id="addExercise">Add to Fitness Routine</button>
-    </form>`);
+        <button class="btn btn-primary" id="addExercise">Add to Workout</button>
+    </form>
+    `);
 }
 
 function writeAllWorkouts(response) {
-    $("#getInShape").html('<h2>Choose your Routine</h2>')
+    $("#getFit").html(`<h2>Choose a workout routine</h2>`)
     for (routine of response) {
-        $("#getInShape").append(`<button class="btn btn-success fitnessBtn" value="${routine._id}">${routine.name}</button>`)
+        $("#getFit").append(`
+            <button class="btn btn-success workoutBtn" value="${routine._id}">${routine.name}</button>
+        `)
     }
-    $(".fitnessBtn").click(event => {
+    $(".workoutBtn").click(event => {
         let workoutID = $(event.currentTarget).val();
-        $("#getInShape").html(`<h2>Current Routine</h2><ul id="exerciseList" class="row"</ul>`)
+        $("#getFit").html(`
+        <h2>Current Routine</h2>
+        <ul id="exerciseList" class="row"></ul>
+        `)
         writeExercises(workoutID)
-        writeExerciseForm(event.currentTarget).val();
+        writeExerciseForm(event.currentTarget)
         $("#addExercise").click((event) => {
             event.preventDefault();
-            let muscGrp = $("input[name*='muscleGroup']").val()
             let numReps = $("input[name*='exerciseReps']").val()
             let newExercise = {
                 workout: workoutID,
-                muscGrp: $("input[name*='muscleGroup']").val().trim(),
                 name: $("input[name*='exerciseName']").val().trim(),
                 reps: (numReps > 0) ? numReps : undefined
             }
@@ -60,11 +61,9 @@ function writeAllWorkouts(response) {
                     clearTimeout(alertTimeout);
                     if (result.errors != undefined) {
                         if (result.errors.name) {
-                            $(".alert-warning").text("What exercise would you like to contribute?").attr('style', 'display:block;')
-                        } else if (result.errors.muscGrp) {
-                            $(".alert-warning").text("What muscle group would you like to add?").attr('style', 'display:block;')
+                            $(".alert-warning").text("Please enter the exercise you want to add.").attr('style', 'display:block;')
                         } else if (result.errors.reps) {
-                            $(".alert-warning").text("How many reps would you like to add?").attr('style', 'display:block;')
+                            $(".alert-warning").text("Please enter the number of reps.").attr('style', 'display:block;')
                         }
                         alertTimeout = setTimeout(() => {
                             $(".alert-warning").attr('style', 'display:none;')
@@ -88,15 +87,16 @@ $("#loadWorkout").click(() => {
     })
 });
 
-$("#getInShape").click(() => {
-    $("#getInShape").html(`
+$("#newWorkout").click(() => {
+    $("#getFit").html(`
     <form action="/submit" method="post">
         <div class="form-group">
-            <label for="workoutName">Name of the routine</label>
-            <input class="form-control" type="text" name="workoutName">
+            <label for="workoutName">Name of Workout Routine</label>
+            <input class="form-control" type="text" name="workoutName" value="" placeholder="Workout Title">
         </div>
         <button class="btn btn-primary" id="createWorkout">Submit</button>
-    </form>`);
+    </form>
+    `);
     $("#createWorkout").click((event) => {
         event.preventDefault();
         $.ajax({
@@ -108,7 +108,7 @@ $("#getInShape").click(() => {
                 if (result != 'Workout validation failed') {
                     location.reload();
                 } else {
-                    $(".alert-warning").text("Please add the name of your routine.").attr('style', 'display: block;')
+                    $(".alert-warning").text("Please enter the name of your new workout routine.").attr('style', 'display:block;')
                     alertTimeout = setTimeout(() => {
                         $(".alert-warning").attr('style', 'display:none;')
                     }, 4000)
